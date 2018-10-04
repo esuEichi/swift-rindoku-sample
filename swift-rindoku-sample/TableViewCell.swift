418a9727c51a8763d4215153609a77f700f0d999
esuEichi <aloneware94@gmail.com>
2018-10-04 11:17:04 +0900

    tableview

---
+++ swift-rindoku-sample/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  swift-rindoku-sample
+//
+//  Created by esu eichi on 2018/09/27.
+//  Copyright © 2018年 hicka04. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
